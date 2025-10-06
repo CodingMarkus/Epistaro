16f29427f411fa94c9900b41e20fb93f67821d72
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2025-10-06 04:15:43 +0200

    Add shared coding helpers
    Those will help you writing better C code

---
+++ shared/src/begin_header.h
@@ -0,0 +1,11 @@
+#include "private/begin_common.h"
+
+
+#define begin_header \
+	_Pragma("clang assume_nonnull begin")
+
+#define end_header \
+	_Pragma("clang assume_nonnull end")
+
+
+#define public __attribute__((__visibility__("default")))
---
+++ shared/src/end_header.h
@@ -0,0 +1,6 @@
+#undef public
+
+#undef begin_header
+#undef end_header
+
+#pragma clang visibility pop
