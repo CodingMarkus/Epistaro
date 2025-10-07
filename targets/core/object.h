e3436325748da88cf0cb166874c5adad09aabcd2
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2025-10-07 12:35:03 +0200

    Add a freeze and hash function, allow shallow copy

--- targets/core/object.h
+++ targets/core/object.h
@@ -4,53 +4,87 @@
 begin_header
 // ============================================================================
 
+typedef uint32_t HashValue_Object;
+
+
 /**
-	Creates a deep copy.
-	Returning a shim object that implements copy-on-write is allowed.
+	Returns a hashed value representing the objects current internal state.
+	If `hashIsDeep` is `true`, the returned value must be the same for two
+	objects that compare equal using their `CompareFunc_Object` function.
+	Yet two non-equal objects may still return the same hash value.
+	If `hashIsDeep` is false, the hash must not include state of any
+	referenced objects but only of the object itself, thus when a referenced
+	object changes in state, the this hash value would not.
+*/
+typedef HashValue_Object (HashFunc_Object)(
+	const void * anyObject, bool hashIsDeep
+);
+
+/**
+	Either create a copy of the object itself (`copyIsDeep` is `false`)
+	or a deep copy of the object and all objects it references
+	(`copyIsDeep` is `true`).
  */
-typedef void * (* CopyFunc_Object)( const void * anyObject );
+typedef void * (CopyFunc_Object)( const void * anyObject, bool copyIsDeep );
+
+/**
+	Freeze all referenced objects (always deep).
+	The object must not freeze itself!
+*/
+typedef void * (FreezeFunc_Object)( const void * anyObject );
+
+/**
+	Performs additional clean-up when an object is about to be destroyed,
+	e.g. discarding objects it refers to.
+*/
+typedef void (DestroyFunc_Object)( const void * anyObject );
 
 /**
 	Compares the object to another one, returns `true` only if both objects are
-	functional equivalent in every aspect.
- */
-typedef bool (* CompareFunc_Object)( const void *_nil anyObject );
-
-/**
-	Performs additional clean-up when an object is about to be destroyed.
- */
-typedef void (* DestroyFunc_Object)( const void * anyObject );
+	functional equivalent in **EVERY** aspect. This may require also comparing
+	referenced objects in case those do change functionality.
+*/
+typedef bool (CompareFunc_Object)( const void *_nil anyObject );
 
 /**
 	Creates a human readable description string of the object.
 	Caller must free returned string.
- */
-typedef char * (* CreateDescFunc_Object)( const void * anyObject );
+*/
+typedef char * (CreateDescFunc_Object)( const void * anyObject );
 
 
 /**
 	@param name Name of the object as printable string. For the same object
 		type, it's not sufficient that this string has the same "value", it
 		actually must be the same string (== comparison must be true!).
+	@param hashFunc Function to calculate the hash of an object.
+		See `HashFunc_Object` for details.
 	@param copyFunc Function to copy the object. If the object is immutable,
 		just set to `nil` and the object is never copied but just retained.
 		If the object can be copied by just cloning it byte for byte, set it
-		to `CloneCopyFunc_Object`.
+		to `CloneCopyFunc_Object`. See `CopyFunc_Object` for details.
+	@param freezeFunc Function to freeze all objects this object refers to.
+		If the object does not refer to any other objects, just set to `nil`.
+		See `FreezeFunc_Object` for details.
 	@param destroyFunc Function to perform additional clean-up when the object
 		is about to be destroyed. Set it to `nil` if no clean up is required
-		other than freeing the object's memory.
+		other than freeing the object's memory. See `DestroyFunc_Object` for
+		details.
 	@param compareFunc Function to compare the object. If the object can be
-		compared by just using `memcmp()`, set it to `nil`.
+		compared by just using `memcmp()`, set it to `nil`. See
+		`CompareFunc_Object` for details.
 	@param createDescFunc Function creates a human readable description of
 		the object. Useful for logging and debugging. Caller must free returned
-		string.
+		string. See `CreateDescFunc_Object` for details.
 */
 struct ObjectType {
 	const char * name;
-	CopyFunc_Object _nil copyFunc;
-	DestroyFunc_Object _nil destroyFunc;
-	CompareFunc_Object _nil compareFunc;
-	CreateDescFunc_Object createDescFunc;
+	HashFunc_Object * hashFunc;
+	CopyFunc_Object *_nil copyFunc;
+	FreezeFunc_Object *_nil freezeFunc;
+	DestroyFunc_Object *_nil destroyFunc;
+	CompareFunc_Object *_nil compareFunc;
+	CreateDescFunc_Object * createDescFunc;
 };
 
 // ----------------------------------------------------------------------------
